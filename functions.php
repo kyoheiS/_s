0547e60a0b16fa1c572b70419bee5db15ef9c2d7
KyoheiOrg <tgifz5jmr@gmail.com>
2022-03-02 11:25:38 +0900

    リセットCSSの追加,jsの読み込み処理の追加

--- functions.php
+++ functions.php
@@ -142,6 +142,7 @@ function _s_scripts() {
 	wp_style_add_data( '_s-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'main_script', get_template_directory_uri() . '/dist/main.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
